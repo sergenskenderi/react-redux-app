8158754abde1d70eaffcab5f2db8499cb5544692
sergiskenderi <sergi.skenderi@idra.al>
2021-06-15 17:07:58 +0200

    draft 1

---
+++ src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchForm = () => {
+    return(
+        <form>
+          <input />
+          <button>Search</button>
+        </form>
+    )
+}
+
+export default SearchForm;
